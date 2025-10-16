d8e3a06954261b23abc09c97faacca0bd88ddcb5
Loky1280 <149267171+Loky1280@users.noreply.github.com>
2025-10-16 19:32:05 +0300

    Add files via upload

---
+++ General.js
@@ -0,0 +1,11 @@
+const burgerMenu = document.getElementById("BurgerMenu");
+const centralBurgerLine = document.getElementsByClassName("CentralBurgerElement");
+
+burgerMenu.addEventListener("change", () => {
+    if(burgerMenu.checked){
+        centralBurgerLine[0].style.width = "100%";
+        
+    }else{
+        centralBurgerLine[0].style.width = "70%";
+    }
+});
